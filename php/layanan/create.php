8d83d2c0df0c416b14bfaf7e1bf252564c7f4a87
Rangga Ali Ardyansyah <122335520+ranggali@users.noreply.github.com>
2025-07-12 02:36:41 +0700

    Create create.php

---
+++ php/layanan/create.php
@@ -0,0 +1,24 @@
+<?php
+$conn = new mysqli("localhost", "root", "", "layanan");
+
+if ($conn->connect_error) {
+    die("Koneksi Gagal: " . $conn->connect_error);
+}
+
+$id_poli = $_POST["id_poli"];
+$nama_poli = $_POST["nama_poli"];
+$detail = $_POST["detail"];
+$data = mysqli_query($conn, "INSERT INTO poliklinik SET id_poli='$id_poli', nama_poli='$nama_poli', detail='$detail'");
+
+if ($data) {
+    echo json_encode([
+        'pesan' => 'sukses'
+    ]);
+} else {
+    echo json_encode([
+        'pesan' => 'gagal'
+    ]);
+}
+
+$conn->close();
+?>
